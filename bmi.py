f121d885cd91d87ae91fad239c4aeecf2868003e
AlbertTorres <alberttorres05.22@ichat.sp.edu.sg>
2023-10-24 10:12:11 +0800

    BMI classification

---
+++ bmi.py
@@ -0,0 +1,17 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight/(height**2)
+    print("BMI is " + str(round(bmi,2)))
+    if bmi < 18.5:
+        print("User is underweight")
+    elif 18.5 <= bmi <= 25.0:
+        print("User is normal weight")
+    elif 25.0 < bmi:
+        print("User is Over weight")
+
+
+
+
+calculate_bmi(weight=57, height=1.73)
+
