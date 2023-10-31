ef5c47267b1f3ed4f864d03e001930e90a09b14e
AlbertTorres <alberttorres05.22@ichat.sp.edu.sg>
2023-10-31 09:52:34 +0800

    bmi now returns -1, 0 or 1

--- bmi.py
+++ bmi.py
@@ -5,10 +5,13 @@ def calculate_bmi(height, weight):
     print("BMI is " + str(round(bmi,2)))
     if bmi < 18.5:
         print("User is underweight")
+        return -1
     elif 18.5 <= bmi <= 25.0:
         print("User is normal weight")
+        return 0
     elif 25.0 < bmi:
         print("User is Over weight")
+        return 1
 
 
 
